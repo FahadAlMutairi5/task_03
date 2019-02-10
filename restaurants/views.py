35e1b35d02837f0e5cd4a37b541df2e7eb1e8627
FahadAlMutairi5 <almteref@gmail.com>
2019-02-10 16:22:43 +0300

    Task #3 done

--- restaurants/views.py
+++ restaurants/views.py
@@ -7,6 +7,29 @@ def restaurant_list(request):
 
     context = {
 
+    		"my_list" : [
+
+    					{
+    						"restaurant_name" : "Hr",
+    						"food_type" : "Ch",
+    					},
+
+    					{
+    						"restaurant_name" : "kn",
+    						"food_type" : "Me",
+    					},
+
+    					{
+    						"restaurant_name" : "Sd",
+    						"food_type" : "Re",
+    					},
+
+    					{
+    						"restaurant_name" : "We",
+    						"food_type" : "Qe",
+    					},
+    		]
+
     }
     return render(request, 'list.html', context)
 
@@ -14,6 +37,10 @@ def restaurant_list(request):
 def restaurant_detail(request):
 
     context = {
+    		"my_object" : {
 
+    					"restaurant_name" : "Favorite Re",
+    					"food_type" : "Check",
+    		}
     }
     return render(request, 'detail.html', context)
